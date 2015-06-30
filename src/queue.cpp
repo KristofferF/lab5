3bbe277d9f18a75072e287f3d1e8c216e22ad4b5
Kristoffer Freiholtz <Kristoffer.Freiholtz@hotmail.com>
2015-07-01 00:01:32 +0200

    Fixed .h and .cpp file

--- src/queue.cpp
+++ src/queue.cpp
@@ -5,11 +5,54 @@
  *      Author: Kristoffer
  */
 
+#include "queue.h"
+
 class Node
 {
 public:
-Node *next;
-Item data;
-Node (Node *n, Item newData) : next(n), data(newData) {}
+    Node *next;
+    Item data;
+    Node (Node *n, Item newData) : next(n), data(newData) {}
 };
+
+QIterator::QIterator(){
+
+}
+
+QIterator::QIterator(Node* n){
+
+}
+
+Item &QIterator::operator*() const{
+
+}
+
+QIterator &QIterator::operator++(){
+
+}
+
+QIterator QIterator::operator++(int){
+
+}
+
+bool QIterator::operator!=(const QIterator &qi) const{
+
+}
+
+QList::~QList(){
+
+}
+void QList::enque(Item item){
+
+}
+bool QList::deque(Item &item){
+
+}
+bool QList::del(Item item){
+
+}
+bool QList::isEmpty()const{
+
+}
+
 // Fyll på med funktionsdefinitioner för medlemsfunktionerna i QIterator och // QList nedan!
--- src/queue.h
+++ src/queue.h
@@ -8,33 +8,35 @@
 #ifndef QUEUE_H_
 #define QUEUE_H_
 
+#include "person.h"
+
 typedef Person Item;
 class Node;
 class QIterator
 {
-  private:
+private:
     Node *node;
-  public:
-    QIterator();
-    QIterator(Node *n);
-Item &operator*() const;
-QIterator &operator++();
-QIterator operator++(int); // postfix i++ bool operator!=(const QIterator &qi) const;
-}; //--------------------------------------------------------------------------- class QList
+public:
+    QIterator(); // Förvald konstruktor
+    QIterator(Node *n); // Initieringskonstruktor
+    Item &operator*() const;
+    QIterator &operator++(); // prefix ++i
+    QIterator operator++(int); // postfix i++
+    bool operator!=(const QIterator &qi) const;
+}; //---------------------------------------------------------------------------
+
+class QList
 {
 private:
-     Node *first, *last;
-   public:
-QList():first(nullptr),last(nullptr){}; ~QList();
-void enque(Item item);
-bool deque(Item &item);
-     bool del(Item item);
-     bool isEmpty()const;
-QIterator begin()const {return QIterator(first);}
-QIterator end() const { return QIterator(nullptr);}
+   Node *first, *last;
+public:
+    QList():first(nullptr),last(nullptr){}; ~QList();
+    void enque(Item item);
+    bool deque(Item &item);
+    bool del(Item item);
+    bool isEmpty()const;
+    QIterator begin()const {return QIterator(first);}
+    QIterator end() const { return QIterator(nullptr);}
 };
-
-
-
 
 #endif /* QUEUE_H_ */
